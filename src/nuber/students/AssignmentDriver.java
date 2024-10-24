8d58bb47e6e8f0a2b96797b8993327c4b398d547
Gan-Serene <116237097+Gan-Serene@users.noreply.github.com>
2024-10-24 13:15:44 +1030

    edit command in main

--- src/nuber/students/AssignmentDriver.java
+++ src/nuber/students/AssignmentDriver.java
@@ -11,13 +11,12 @@ public class AssignmentDriver {
 		//use the logEvent function to print out debug output when required.
 		boolean logEvents = true;
 		
+		/* //initial testing
 		HashMap<String, Integer> testRegions = new HashMap<String, Integer>();
 		testRegions.put("North Region", 50);
 		
-		/**
 		 * This driver has a number of different sections that you can uncomment as you progress through the assignment
 		 * Once you have completed all parts, you should be able to run this entire function uncommented successfully
-		 */
 
 		Passenger testPassenger = new Passenger("Alex", 100);
 
@@ -50,7 +49,7 @@ public class AssignmentDriver {
 
 		//shutdown the dispatch when it's done
 		dispatch.shutdown();
-		System.out.println("done testing");
+		System.out.println("done testing");*/
 		
 		
 		//create NuberDispatch for given regions and max simultaneous jobs per region
@@ -60,8 +59,8 @@ public class AssignmentDriver {
 		regions.put("North", 50);
 		regions.put("South", 50);
 		
-		new Simulation(regions, 1, 10, 1000, logEvents);
-		//new Simulation(regions, 5, 10, 1000, logEvents);
+		//new Simulation(regions, 1, 10, 1000, logEvents);
+		new Simulation(regions, 5, 10, 1000, logEvents);
 		//new Simulation(regions, 10, 10, 1000, logEvents);
 		//new Simulation(regions, 10, 100, 1000, logEvents);
 		//new Simulation(regions, 1, 50, 1000, logEvents);
